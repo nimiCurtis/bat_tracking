88b42852d76271f3b216cdb5c8ad97a9bfe3f822
nimiCurtis <nimicu21@gmail.com>
2024-09-28 15:19:27 +0300

    ignore

--- models/inference-script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sleap-track /home/roblab20/dev/sleap_bat_tracking/dataset/labels.v001.slp --only-labeled-frames -m models/v1240924_184631.centroid -m models/v1240924_184631.centered_instance --max_instances 1 -o labels.v001.slp.predictions.slp --verbosity json --no-empty-frames
--- models/train-script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sleap-train centroid.json labels.v001.slp
-sleap-train centered_instance.json labels.v001.slp
